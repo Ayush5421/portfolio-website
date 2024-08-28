8cfa4e488225a3043aabb0dee08902d0b0c0ae2c
Ayush Mukherjee <111524872+Ayush5421@users.noreply.github.com>
2024-08-29 00:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('.portfolio-button').mousedown(function(){
     timeout = setInterval(function(){
-        window.scrollBy(0,-1); // May need to be -1 to go down
-    }, 0); // Play around with this number. May go too fast
+        window.scrollBy(0,-1); 
+    }, 0);
 
     return false;
 });
